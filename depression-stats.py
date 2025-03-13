bb65cda6e74ed0aebbea60ab068c481df5db7dde
Lucas Simidu <lucas.simidu@outlook.com>
2025-03-13 00:41:10 -0300

    FEAT: Uploading the Flask content

--- depression-stats.py
+++ depression-stats.py
@@ -1,16 +1,18 @@
 import pandas as pd
-from matplotlib.pyplot import plt
+from matplotlib.pyplot import plot
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-depression_table = "spreadsheets/taxa-depressao-regiao.csv"
+depression_table = "https://raw.githubusercontent.com/ldsimidu/music-effects-on-psychology/refs/heads/main/spreadsheets/taxa-depressao-regiao.csv"
+#https://raw.githubusercontent.com/ldsimidu/music-effects-on-psychology/refs/heads/main/spreadsheets/taxa-depressao-regiao.csv
 
 @app.route("/")
 def show_table():
     df = pd.read_csv(depression_table)
-    total_table = df[["Times","TOTAL"]].to_html(classes="table table-stripped")
+    total_table = df[["Região","Total de Casos de Depressão","Percentual de Depressão","População Total (2019)"]].to_html(classes="table table-stripped")
     
     return render_template("index.html", tabela=total_table)
 
-
+if __name__ == "__main__":
+    app.run(debug=True)
