693e9a94c541d09750822d63a5772c014a1f5f85
Lucas Simidu <lucas.simidu@outlook.com>
2025-03-13 01:26:51 -0300

    FEAT: Adding "total_casos" and "percentual_total" filter tables

--- depression-stats.py
+++ depression-stats.py
@@ -5,14 +5,28 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 depression_table = "https://raw.githubusercontent.com/ldsimidu/music-effects-on-psychology/refs/heads/main/spreadsheets/taxa-depressao-regiao.csv"
-#https://raw.githubusercontent.com/ldsimidu/music-effects-on-psychology/refs/heads/main/spreadsheets/taxa-depressao-regiao.csv
 
 @app.route("/")
-def show_table():
+def show_total_table():
     df = pd.read_csv(depression_table)
     total_table = df[["Região","Total de Casos de Depressão","Percentual de Depressão","População Total (2019)"]].to_html(classes="table table-stripped")
     
     return render_template("index.html", tabela=total_table)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+@app.route("/total_casos")
+def show_total_cases():
+    df = pd.read_csv(depression_table)
+    total_cases = df[["Região","Total de Casos de Depressão", "População Total (2019)"]].to_html(classes="table table-stripped")
+
+    return render_template("index.html", tabela=total_cases)
+
+
+@app.route("/percentual_total")
+def show_percent_table():
+    df = pd.read_csv(depression_table)
+    percent_table = df[["Região","Percentual de Depressão","População Total (2019)"]].to_html(classes="table table-stripped")
+    
+    return render_template("index.html", tabela=percent_table)
+
+app.run(debug=True)
